02bcd91064b4d6e0c82c29ff4eda3cb5a3ba834d
Prosper Ugbovo [YONGDEV] <85100001+prosperyong@users.noreply.github.com>
2024-10-21 13:12:44 +0100

    :hammer: refactor: fixed some errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
-const animate = require("tailwindcss-animate")
+
+import animate from 'tailwindcss-animate'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   safelist: ["dark"],
   prefix: "cz",
-  
+
   content: [
     './pages/**/*.{ts,tsx,vue}',
     './components/**/*.{ts,tsx,vue}',
     './app/**/*.{ts,tsx,vue}',
     './src/**/*.{ts,tsx,vue}',
 	],
-  
+
   theme: {
     container: {
       center: true,
